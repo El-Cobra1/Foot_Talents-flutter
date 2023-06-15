e5d347f59886afd6cd78d41070e17e29644e10c0
SAMSUNG <adelkutait8@gmail.com>
2023-06-15 23:28:51 +0300

    Hi10

--- lib/models/team_stats.dart
+++ lib/models/team_stats.dart
@@ -2,12 +2,12 @@ import 'package:foottalents/Models/team.dart';
 
 class TeamStats extends Team{
 
-  int playedMatches;
-  int won;
-  int loss;
-  int GF;// goal for
-  int GA;// goal against
+  final int playedMatches;
+  final int won;
+  final int loss;
+  final int GF;// goal for
+  final int GA;// goal against
 
-  TeamStats({required won,required super.id, required super.name, required super.imageUrl, required super.country, required super.city, required super.createDate, required super.beforePointScore, required super.currentPointScore, required super.status});
+  TeamStats(this.playedMatches, this.won, this.loss, this.GF, this.GA, {required super.id, required super.name, required super.imageUrl, required super.country, required super.city, required super.createDate, required super.beforePointScore, required super.currentPointScore, required super.status});
 
 }
